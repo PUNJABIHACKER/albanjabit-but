ed505692dbd7c263ed177b53ac54d1c67a28c068
PUNJABIHACKER <53191562+PUNJABIHACKER@users.noreply.github.com>
2019-07-22 14:13:39 -0400

    asd

---
+++ altatbiqia.js
@@ -0,0 +1,5 @@
+var Webhook = require("webhook-discord")
+
+var Hook = new Webhook("https://discordapp.com/api/webhooks/602926173114204180/R7ll901wDZhXTMmE8-4QoRbyHimUa9bk0fkuGqu-zZQSdGGkJi6MRpIwgc5UdNc211TG")
+
+Hook.success("أنا أحب الاباحية","أنا أحب الاباحية")
